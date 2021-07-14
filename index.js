1175dc7ae97771384f8c121b1184345449db10d6
LucileGB <72474020+LucileGB@users.noreply.github.com>
2021-07-14 23:44:43 +0200

    Add files via upload
    -  Proper loading of next/previous pages.
    -  Clearer template formatting with mustaches.js
    -  Code cleaning

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const URL_NEWISH = "http://localhost:8000/api/v1/titles/?year=&min_year=2000&sor
 const URL_OLDIES = "http://localhost:8000/api/v1/titles/?year=&max_year=1999&sort_by=-imdb_score";
 const URL_WORST = 'http://localhost:8000/api/v1/titles/?sort_by=imdb_score'
 
+// Stock the loaded movie lists
 let dataArrays = {};
+/* For each category, Stocks the current API page reached and the index
+of the last displayed movie in the corresponding dataArrays list */
+let ocMoviePositionTracking = {};
 
-alert("Félicitations !! Vuos êtes le 666 VISITEUR SUR NOTRE SITE !!! Cliquez sur ce lien fiable pour gagner une volvo :");
-
-function difference(value1) {//Calculates the difference between a number and a multiple of five.
-  if (value1%5 != 0) {
-    return 0;
-  } else {
-    for (i = 0; i < 5; i++) {
-      if ((value1 += 1)%5 == 0) {
-        return 5-(i+1);
-      }
-    }
-  }
-}
 
 function prettyList(list) {//Formats a list with a space after commas
   var listString = "";
@@ -26,7 +17,7 @@ function prettyList(list) {//Formats a list with a space after commas
   if (list.length == 1) {
     return list;
   }
-  for (i = 0; i < listLenght; i++) {
+  for (var i = 0; i < listLenght; i++) {
     var item = list[i];
     if (i == 0) {
       listString = listString + item;
@@ -36,35 +27,68 @@ function prettyList(list) {//Formats a list with a space after commas
   }
   return listString;
 }
+
+
 function formatIndex(movie) {// Formats movie cover for display on the index
   var title = movie["title"];
-  var poster = movie["image_url"];
+  var cover = movie["image_url"];
   var idMovie = movie["id"];
-  var result = `<p><input type="image" class="cover-index" src="${poster}" alt="${title} - Cliquez pour en savoir plus !" id="${idMovie}" onclick="openModal('${TITLE_URL}${idMovie}')"/></p>`;
+  var result = `<p><input type="image" class="cover-index" src="${cover}" alt="${title} - Cliquez pour en savoir plus !" id="${idMovie}" onclick="openModal('${TITLE_URL}${idMovie}')"/></p>`;
   return result;
 }
 
 function formatModal(cover, title, genres, year, rated, imdb_score, directors,
                     actors, duration, countries, boxOffice, summary) {
 // Format the movie informations for display on the modal
-  var result = `<h2 class="mod-title">${title}</h2><p><img class="mod-cover" img src="${cover}" alt="Poster de ${title}"/></p><div class="mod-data"><p><b>Genres : </b>${genres}<br><b>Résumé : </b>${summary}<br><b>Année : </b>${year}<br><b>Score : </b>${rated}<br><b>Score Imdb : </b>${imdb_score}<br><b>Réalisateur(s) : </b>${directors}<br><b>Acteurs : </b>${actors}<br><b>Durée : </b>${duration}<br><b>Pays d'origine : </b>${countries}<br><b>Box-office : </b> ${boxOffice}</p></div>`;
+  var data = { title: `<h2 class='mod-title'>${title}</h2>`,
+              cover: `<p><img class="mod-cover" img src="${cover}" alt="Poster de ${title}"/></p>`,
+              genres: `<b>Genres : </b>${genres}<br>`,
+              summary: `<b>Résumé : </b>${summary}<br>`,
+              year: `<b>Année : </b>${year}<br>`,
+              rated: `<b>Score : </b>${rated}<br>`,
+              imdb: `<b>Score Imdb : </b>${imdb_score}<br>`,
+              directors: `<b>Réalisateur(s) : </b>${directors}<br>`,
+              actors: `<b>Acteurs : </b>${actors}<br>`,
+              duration: `<b>Durée : </b>${duration}<br>`,
+              countries: `<b>Pays d'origine : </b>${countries}<br>`,
+              boxOffice: `<b>Box-office : </b> ${boxOffice}`,
+              };
+  var template = `{{{ title }}}<div id="mod-data">{{{ cover }}}<p>{{{ genres }}} {{{ summary }}} {{{ year }}} {{{ rated }}} {{{ imdb }}}{{{ actors }}}{{{ duration }}}{{{ countries }}}{{{ boxOffice }}}</p></div>`;
+  var result = Mustache.render(template, data);
   return result;
 }
+
+async function pageCount(path) {// Return the last API page accessed for a category
+  var numberOfpages;
+  await axios.get(path).then(
+    (response) => {
+      var result = response.data;
+      numberOfpages = Math.ceil(result["count"] /= 5);
+
+    },
+    (error) => {
+       alert("Une erreur est survenue pendant le chargement de la page. Veuillez actualiser.")
+    }
+  );
+  return numberOfpages;
+}
+
+
 
 async function openModal(path) {
 // Opens the modal with the necessary informations, when available.
   await axios.get(path).then(
     (response) => {
       var result = response.data;
-      rated = result["rated"];
-      worldwide = result["worldwide_gross_income"];
-      description = result["description"];
-      cover = result["image_url"];
-      duration = `${result["duration"]} minutes`;
-      genres = prettyList(result["genres"]);
-      directors = prettyList(result["directors"]);
-      actors = prettyList(result["actors"]);
-      countries = prettyList(result["countries"]);
+      var rated = result["rated"];
+      var worldwide = result["worldwide_gross_income"];
+      var description = result["description"];
+      var cover = result["image_url"];
+      var duration = `${result["duration"]} minutes`;
+      var genres = prettyList(result["genres"]);
+      var directors = prettyList(result["directors"]);
+      var actors = prettyList(result["actors"]);
+      var countries = prettyList(result["countries"]);
 
       if (rated == "Not rated or unkown rating") {
         rated = "Information non disponible.";
@@ -97,7 +121,7 @@ async function openModal(path) {
 }
 
 async function getTopMovie() {//Gets the best movie
-  movie = ""
+  var movie = ""
     await axios.get(URL_BEST).then(
       (response) => {
         var result = response.data;
@@ -115,10 +139,16 @@ async function showTopMovie(movieId) {//Show the best movie
     (response) => {
       var movie = response.data;
       var title = movie["title"];
-      var poster = movie["image_url"];
+      var cover = movie["image_url"];
       var idMovie = movie["id"];
       var description = movie["description"];
-      var formatted = `<div><h3>${title}</h3><p><input type="image" src="img/btn_play.png" alt="Cliquez ici pour streamer ${title}!" id="btn-play"/></p><p>${description}</p></div><div><p><input type="image" class="cover-index" src="${poster}" alt="${title} - Cliquez pour en savoir plus !" id="${idMovie}" onclick="openModal('${TITLE_URL}${idMovie}')"/></p></div>`;
+      var data = {title: `<h3>${title}</h3>`,
+                  button: `<p><input type="image" src="img/btn_play.png" alt="Cliquez ici pour streamer ${title}!" id="btn-play"/></p>`,
+                  cover: `<p><input type="image" class="cover-index" src="${cover}" alt="${title} - Cliquez pour en savoir plus !" id="${idMovie}" onclick="openModal('${TITLE_URL}${idMovie}')"/></p>`,
+                  description: `<p>${description}</p>`,
+                };
+      var template = `<div>{{{ title }}}{{{ button }}}{{{ description }}}</div><div>{{{ cover }}}</div>`;
+      var formatted = Mustache.render(template, data);
       var formattedBloc = document.getElementById('top-movie');
       formattedBloc.innerHTML = formatted;
     },
@@ -128,21 +158,13 @@ async function showTopMovie(movieId) {//Show the best movie
 );
 }
 
-async function getMovies(path, listLength) {// Load movies.
+async function getMovies(path, page) {// Load movies.
   var listMovies = [];
   var listResults = "";
-  var page = "";
-  var gap = 0;
-  if (listLength == 0) {
-    page = 0;
-  } else {//If there's already a loaded list, calculate the position of the first movie to be loaded next.
-    page = Math.ceil(listLength /= 5);
-    gap = difference(listLength);
-  }
-
-  for (i = 0; i < 14; i++) {
-    page ++;
-    var currentpath = `${path}&page=${page}`;
+  var startPage = page;
+  for (var i = 0; i < 14; i++) {
+    startPage ++;
+    var currentpath = `${path}&page=${startPage}`;
     await axios.get(currentpath).then(
         (response) => {
           var result = response.data;
@@ -152,8 +174,8 @@ async function getMovies(path, listLength) {// Load movies.
             return error;
                 }
     );
-    for (j = 0; j < 5; j++){//Load all the movies inside a page
-      if (i == 1 && j < gap) {//... While avoiding movies already loaded, if starting
+    for (var j = 0; j < 5; j++){//Load all the movies inside a page
+      if (listResults[j] == undefined) {//... While avoiding movies already loaded, if starting mid-page
         continue;
       } else {
       listMovies.push({'title': listResults[j]["title"],
@@ -166,12 +188,22 @@ async function getMovies(path, listLength) {// Load movies.
   return listMovies;
 }
 
-function showMovies(list, div) {//Displays movies in the index
+function showMovies(list, div) {//Displays movies to the user
+  var l = list.length;
+  var movieDesc = "";
   var futureInner = "";
   var block = document.getElementById(div);
-  for (let i = 0; i < 7; i++) {
-    movieDesc = formatIndex(list[i]);
-    futureInner = futureInner.concat(" ", movieDesc);
+
+  if (l < 7) {
+    for (var i = 0; i < l; i++) {
+      movieDesc = formatIndex(list[i]);
+      futureInner = futureInner.concat(" ", movieDesc);
+    }
+  } else {
+    for (var i = 0; i < 7; i++) {
+      movieDesc = formatIndex(list[i]);
+      futureInner = futureInner.concat(" ", movieDesc);
+    }
   }
   block.innerHTML = futureInner;
 }
@@ -189,64 +221,91 @@ function onLoading() {//Loads the page, including the 70 first movies of each li
                   "newish": result3,
                   "oldies": result4,
                   "worst": result5};
+
+    /* We save the current API page, and the index of the last loaded movie
+    which is in the dataArrays list *///
+    Promise.all([pageCount(TITLE_URL), pageCount(URL_NEWISH), pageCount(URL_OLDIES),
+                pageCount(URL_WORST)])
+    .then(([result1, result2, result3, result4]) => {
+      ocMoviePositionTracking = {"best-rated-pos": [14, 6],
+                      "newish-pos": [14, 6],
+                      "oldies-pos": [14, 6],
+                      "worst-pos": [14, 6],
+                      "best-rated-count": result1,
+                      "newish-count": result2,
+                      "oldies-count": result3,
+                      "worst-count": result4};
+    },
+    (error) => {
+      alert(`Une erreur est survenue pour la raison suivante : ${error}. Merci de recharger la page.`);
+    }
+  );
   },
   (error) => {
-  //Peut-on gérer une erreur pour chaque catégorie sans avoir à C/C ?
-    alert(error);
+  //NOTE : Peut-on gérer une erreur pour chaque catégorie sans avoir à C/C ?
+    alert(`Une erreur est survenue pour la raison suivante : ${error}. Merci de recharger la page.`);
   }
   );
+
 }
 
-function previousPage(buttonId) {//Charge la page précédente
-  var div = document.getElementById(buttonId);
-  var firstMovie = div.getElementsByClassName("cover-index")[0];
-  var movieId = firstMovie.id;
-  var movieIndex = 0;
-  for (let movie of dataArrays[buttonId]) {
-    if (movie["id"] == movieId) {
-      movieIndex = dataArrays[buttonId].indexOf(movie);
-      break;
-    }
-  }
-  if (movieIndex == 0) {
+function previousPage(buttonId) {//Load previous page
+  var lastLoadedIndex = ocMoviePositionTracking[`${buttonId}-pos`][1];
+
+  if (lastLoadedIndex == 6) {
     return 0;
   } else {
-    var toLoad = dataArrays[buttonId].slice(movieIndex-7, movieIndex);
+    var toLoad = loadList(buttonId, lastLoadedIndex -= 14);
     showMovies(toLoad, buttonId);
+    ocMoviePositionTracking[`${buttonId}-pos`][1] = lastLoadedIndex += 7;
   }
 }
 
-async function nextPage(buttonId, url) {
-  var div = document.getElementById(buttonId);
-  var seventhMovie = div.getElementsByClassName("cover-index")[6];
-  var movieId = seventhMovie.id;
-  var movieIndex = 0;
+function loadList(list, movieIndex) {//Load a list for nextPage
+  var result = [];
+  var start = movieIndex += 1;
+  for (var i = 0; i < 7; i++) {
+    result.push(dataArrays[list][start]);
+    start++;
+  }
+  return result;
+}
+
+function nextPage(buttonId, url) {
+  var totalPages = ocMoviePositionTracking[`${buttonId}-count`];
+  var lastAPIPage = ocMoviePositionTracking[`${buttonId}-pos`][0];
+  var lastLoadedIndex = ocMoviePositionTracking[`${buttonId}-pos`][1];
   var listLength = dataArrays[buttonId].length;
-  for (let movie of dataArrays[buttonId]) {//Gives us our starting index to load
-    if (movie["id"] == movieId) {
-      movieIndex = dataArrays[buttonId].indexOf(movie);
-      break;
+  var toLoad = [];
+
+  if (lastAPIPage == totalPages) {// If we reached the end, doesn't change
+    return 0;
+  } else {// Loads the next movies in the global
+    toLoad = loadList(buttonId, lastLoadedIndex);
+    showMovies(toLoad, buttonId);
+
+//    if (lastLoadedIndex > (listLength -= 54) && lastLoadedIndex < (listLength -= 10))
+    if (lastLoadedIndex > (listLength -= 10))
+    {//Loads new movies if we're nearing the end of the loaded list
+      Promise.all([getMovies(url, lastAPIPage)])
+      .then(([result1]) => {
+        dataArrays[buttonId] = dataArrays[buttonId].concat(result1);
+
+        /* We update the new list values. Should we decide to use a number other than 70
+        for the number of loaded movies, we'll have to use Math.floor and adjust page count. */
+        var newListLength = dataArrays[buttonId].length;
+        var newAPIPage = newListLength /= 5;
+        ocMoviePositionTracking[`${buttonId}-pos`][0] = newAPIPage;
+      },
+      (error) => {
+        alert(`Une erreur est survenue pour la raison suivante : ${error}. Merci de recharger la page.`);
+      }
+    );
     }
   }
-  await axios.get(url).then(
-    (response) => {
-      var result = response.data;
-      var pageCount = Math.ceil(result["count"] /= 5);
-      if (movieIndex > (pageCount -= 9)) {
-        return 0;
-      } else {
-        var toLoad = dataArrays[buttonId].slice(movieIndex+1, movieIndex+9);
-        showMovies(toLoad, buttonId);
-      }
-      if (movieIndex > (listLength -= 18)) {
-        newMoviesList = getMovies(url, listLength);
-        dataArrays[buttonId] = dataArrays[buttonId] + newMoviesList;
-      }
-    }),
-    (error) => {
-      alert(error);
-    }
 
+//Updating the last loaded movie index
+  ocMoviePositionTracking[`${buttonId}-pos`][1] = lastLoadedIndex + toLoad.length;
 }
 
 onLoading();
